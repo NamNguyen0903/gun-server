2905ebdbbd10ca1269366254030436b2b02b25b3
NamNguyen0903 <namtronghlg0903@gmail.com>
2024-05-04 19:15:50 +0700

    server.js

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
+const express = require('express');
 const Gun = require('gun');
-const http = require('http');
+const port = process.env.PORT || 8081;
 
-const server = http.createServer((req, res) => {
-    if (Gun.serve(req, res)) return; // Phục vụ các file của GunDB
-    res.writeHead(200);
-    res.end('GunDB Server is running!');
+const app = express();
+app.use(Gun.serve);
+app.use(express.static(__dirname));
+
+const server = app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
 });
 
-// Đảm bảo cập nhật địa chỉ IP và cổng cho các máy chủ khác trong mạng của bạn
 const gun = Gun({
     web: server,
-    peers: ['https://dcc3-2402-800-63f2-d427-b979-b645-ad28-9b92.ngrok-free.app/gun', 'http://192.168.1.12:8080/gun']
+    peers: ['https://6aad-2402-800-63f2-d427-b979-b645-ad28-9b92.ngrok-free.app', 'http://remote-address-2:8080/gun', 'http://remote-address-3:8080/gun'] // Cập nhật các địa chỉ thực tế
 });
 
-server.listen(8081, () => {
-    console.log('Server listening on http://localhost:8081');
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
 });
